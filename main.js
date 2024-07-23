32e399167a41ac4298a0dd0b93369ee3092d3ac3
fenroe <scrambles9494@gmail.com>
2024-07-23 15:37:59 +0300

    Created a function to generate a random choice in a game of rock paper scissors

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 alert("Hello world");
+
+const choices = ["rock", "paper", "scissors"];
+const getComputerChoice = () => {
+  const randomChoice = Math.floor(Math.random() * choices.length);
+  return choices[randomChoice];
+};
+
