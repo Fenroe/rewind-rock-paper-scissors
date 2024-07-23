a2fb04d8bd3ed065814ae53ab753609bd1983870
fenroe <scrambles9494@gmail.com>
2024-07-23 15:42:17 +0300

    Created a function to prompt a human decision in a game of rock paper scissors, and created variables to keep track of player scores

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-alert("Hello world");
+let humanScore = 0;
+let computerScore = 0;
 
 const choices = ["rock", "paper", "scissors"];
+
 const getComputerChoice = () => {
   const randomChoice = Math.floor(Math.random() * choices.length);
   return choices[randomChoice];
 };
 
+const getHumanChoice = () => {
+  const humanChoice = prompt("What is your choice?").toLowerCase();
+  if (!choices.includes(humanChoice)) {
+    return alert("You must choice rock, paper or scissors");
+  } else {
+    return humanChoice;
+  }
+};
