7a68fe6d2afa57fb5f57764d6c0d86c785cdc799
fenroe <scrambles9494@gmail.com>
2024-07-24 15:14:45 +0300

    Added function for determining if the match is over and refactored other functions

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let winner = "";
 
 const choices = ["rock", "paper", "scissors"];
 
@@ -64,6 +65,21 @@ const determineWinner = (computerChoice, humanChoice) => {
   }
 };
 
+const checkForMatchWinner = () => {
+  if (humanScore < 5 && computerScore < 5) {
+    return;
+  }
+  if (humanScore >= 5) {
+    winner = "human";
+    postGameHeading.textContent = "The match is over. You win!";
+  } else {
+    winner = "computer";
+    postGameHeading.textContent = "The match is over. You lose!";
+  }
+  gameContainer.classList.add("hidden");
+  postgameContainer.classList.remove("hidden");
+};
+
 const playRound = (humanChoice) => {
   const computerChoice = getComputerChoice();
   const winner = determineWinner(computerChoice, humanChoice);
@@ -79,30 +95,23 @@ const playRound = (humanChoice) => {
   humanScoreHeading.textContent = `Your score: ${humanScore}`;
   computerScoreHeading.textContent = `Computer score: ${computerScore}`;
   roundOutcomeHeading.textContent = `Computer chose ${computerChoice}. ${message}`;
-};
-
-const playGame = () => {
-  let winnerFound = false;
-  while (!winnerFound) {
-    playRound();
-    if (computerScore === 5) {
-      alert("Computer wins the game");
-      winnerFound = true;
-    }
-    if (humanScore === 5) {
-      alert("You win the game");
-      winnerFound = true;
-    }
-  }
+  checkForMatchWinner();
 };
 
 const startGame = () => {
-  pregameContainer.classList.add("hidden");
-  gameContainer.classList.remove("hidden");
   humanScore = 0;
   computerScore = 0;
+  winner = "";
+  humanScoreHeading.textContent = "Your score: 0";
+  computerScoreHeading.textContent = "Computer score: 0";
+  roundOutcomeHeading.textContent = "";
+  pregameContainer.classList.add("hidden");
+  postgameContainer.classList.add("hidden");
+  gameContainer.classList.remove("hidden");
 };
 
 startButton.addEventListener("click", startGame);
 restartButton.addEventListener("click", startGame);
 rockButton.addEventListener("click", () => playRound("rock"));
+paperButton.addEventListener("click", () => playRound("paper"));
+scissorsButton.addEventListener("click", () => playRound("scissors"));
